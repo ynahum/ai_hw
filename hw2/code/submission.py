e86ab93195066780ecb69d23d561c60db06e77e6
Yair Nahum <yair.y.nahum@intel.com>
2022-05-19 19:17:51 +0300

    hw2 1.1 starts working. need improvements on fuel fill

--- hw2/code/submission.py
+++ hw2/code/submission.py
@@ -10,31 +10,41 @@ class AgentGreedyImproved(AgentGreedy):
         taxi = env.get_taxi(taxi_id)
         other_taxi_id = (taxi_id + 1) % 2
         other_taxi = env.get_taxi(other_taxi_id)
+
+        cash_value = taxi.cash - other_taxi.cash
+        fuel_value = taxi.fuel - other_taxi.fuel
+        total_value = cash_value + fuel_value
+
         gas_0 = env.gas_stations[0]
         gas_1 = env.gas_stations[1]
 
         cost_taxi_to_gas0 = manhattan_distance(taxi.position, gas_0.position)
         cost_taxi_to_gas1 = manhattan_distance(taxi.position, gas_1.position)
-        if (cost_taxi_to_gas0 == (taxi.fuel - 1)) or (cost_taxi_to_gas1 == (taxi.fuel - 1)):
-            if cost_taxi_to_gas0 == (taxi.fuel - 1):
-                value = 60 / (cost_taxi_to_gas0 + 1)
+        if (cost_taxi_to_gas0 >= taxi.fuel ) or (cost_taxi_to_gas1 >= taxi.fuel):
+            if cost_taxi_to_gas0 >= taxi.fuel:
+                total_value -= (cost_taxi_to_gas0 - taxi.fuel)
             else:
-                value = 60 / (cost_taxi_to_gas1 + 1)
-            return value
-
+                total_value -= (cost_taxi_to_gas1 - taxi.fuel)
+            return total_value
 
         taxi_pas_selected = None
         md_taxi_to_pass_selected = None
         md_pas_to_dest_selected = None
+        cash_at_drop = None
+        remaining_cost = None
         value_taxi_to_dest_selected = None
-        
+
+        select_dest = 0
+
         if env.taxi_is_occupied(taxi_id):
             # our taxi agent is occupied
             taxi_pas_selected = taxi.passenger
             md_taxi_to_pass_selected = 0
             md_pas_to_dest_selected = \
                 manhattan_distance(taxi.position, taxi_pas_selected.destination)
-            value_taxi_to_dest_selected = 10 - md_pas_to_dest_selected - md_taxi_to_pass_selected
+            cash_at_drop = 2 * manhattan_distance(taxi_pas_selected.position, taxi_pas_selected.destination)
+            remaining_cost = md_pas_to_dest_selected + md_taxi_to_pass_selected
+            value_taxi_to_dest_selected = cash_at_drop - remaining_cost
         else:
             if env.taxi_is_occupied(other_taxi_id):
                 taxi_pas_selected = env.passengers[0]
@@ -42,7 +52,9 @@ class AgentGreedyImproved(AgentGreedy):
                     manhattan_distance(taxi.position, taxi_pas_selected.position)
                 md_pas_to_dest_selected = \
                     manhattan_distance(taxi_pas_selected.position, taxi_pas_selected.destination)
-                value_taxi_to_dest_selected = md_pas_to_dest_selected - md_taxi_to_pass_selected
+                cash_at_drop = 2 * manhattan_distance(taxi_pas_selected.position, taxi_pas_selected.destination)
+                remaining_cost = md_pas_to_dest_selected + md_taxi_to_pass_selected
+                value_taxi_to_dest_selected = cash_at_drop - remaining_cost
             else:
                 # both taxies are not occupied
                 opt_pas_1 = env.passengers[0]
@@ -50,7 +62,7 @@ class AgentGreedyImproved(AgentGreedy):
                     manhattan_distance(opt_pas_1.position, opt_pas_1.destination)
                 md_taxi_to_opt_pas_1 = manhattan_distance(taxi.position, opt_pas_1.position)
                 cost_taxi_to_dest_1 = md_taxi_to_opt_pas_1 + md_opt_pas_1_to_dest
-                taxi_to_dest_1_value = md_opt_pas_1_to_dest - md_taxi_to_opt_pas_1
+                taxi_to_dest_1_value = (md_opt_pas_1_to_dest - md_taxi_to_opt_pas_1)/(cost_taxi_to_dest_1+1)
                 md_other_taxi_to_pas_1 = \
                     manhattan_distance(other_taxi.position, opt_pas_1.destination)
 
@@ -59,8 +71,7 @@ class AgentGreedyImproved(AgentGreedy):
                     manhattan_distance(opt_pas_2.position, opt_pas_2.destination)
                 md_taxi_to_opt_pas_2 = manhattan_distance(taxi.position, opt_pas_2.position)
                 cost_taxi_to_dest_2 = md_taxi_to_opt_pas_2 + md_opt_pas_2_to_dest
-                taxi_to_dest_2_value = md_opt_pas_2_to_dest - md_taxi_to_opt_pas_2
-
+                taxi_to_dest_2_value = (md_opt_pas_2_to_dest - md_taxi_to_opt_pas_2)/(cost_taxi_to_dest_2+1)
                 md_other_taxi_to_pas_2 = \
                     manhattan_distance(other_taxi.position, opt_pas_2.destination)
 
@@ -70,51 +81,58 @@ class AgentGreedyImproved(AgentGreedy):
                 opt_p2_closer_to_taxi_than_other_taxi =\
                     (md_other_taxi_to_pas_2 > md_taxi_to_opt_pas_2)
 
+                # 1 - dest1, 2 - dest2
+                select_dest = 0
                 if opt_p1_closer_to_taxi_than_other_taxi and opt_p2_closer_to_taxi_than_other_taxi:
                     if taxi_to_dest_1_value_is_greater:
-                        select_opt_p1 = True
+                        select_dest = 1
                     else:
-                        select_opt_p1 = False
+                        select_dest = 2
                 elif opt_p1_closer_to_taxi_than_other_taxi:
-                    select_opt_p1 = True
+                    select_dest = 1
                 elif opt_p2_closer_to_taxi_than_other_taxi:
-                    select_opt_p1 = False
+                    select_dest = 2
                 else:
-                    # TODO: other agent has both passengers closer than our agent
-                    # put a constant value?
-                    print("************************TODO: other agent is closer to both")
-                    #currently we select opt2 even though we cannot win the other agent
-                    select_opt_p1 = False
+                    select_dest = 0
+                    print("*** no selection ** value is 0")
 
-                if select_opt_p1:
+                if select_dest == 1:
                     taxi_pas_selected = opt_pas_1
                     md_taxi_to_pass_selected = md_taxi_to_opt_pas_1
                     md_pas_to_dest_selected = md_opt_pas_1_to_dest
-                    value_taxi_to_dest_selected = taxi_to_dest_1_value
-                else:
+                    cash_at_drop = 2 * manhattan_distance(taxi_pas_selected.position, taxi_pas_selected.destination)
+                    remaining_cost = md_pas_to_dest_selected + md_taxi_to_pass_selected
+                    value_taxi_to_dest_selected = cash_at_drop - remaining_cost
+                elif select_dest == 2:
                     taxi_pas_selected = opt_pas_2
                     md_taxi_to_pass_selected = md_taxi_to_opt_pas_2
                     md_pas_to_dest_selected = md_opt_pas_2_to_dest
-                    value_taxi_to_dest_selected = taxi_to_dest_2_value
+                    cash_at_drop = 2 * manhattan_distance(taxi_pas_selected.position, taxi_pas_selected.destination)
+                    remaining_cost = md_pas_to_dest_selected + md_taxi_to_pass_selected
+                    value_taxi_to_dest_selected = cash_at_drop - remaining_cost
+                else:
+                    value_taxi_to_dest_selected = 0
 
-        total_value = value_taxi_to_dest_selected + taxi.cash
+        total_value += value_taxi_to_dest_selected
 
         debug_prints = True
         if debug_prints:
-            print("--------------------------------------------------")
             print(f"taxi.position: {taxi.position}")
-            print(f"taxi_pas_selected.position: {taxi_pas_selected.position}")
+            if select_dest != 0:
+                print(f"taxi_pas_selected.position: {taxi_pas_selected.position}")
             print(f"taxi is occupied: {env.taxi_is_occupied(taxi_id)}")
 
             print(f"md_taxi_to_pass_selected: {md_taxi_to_pass_selected}")
             print(f"md_pas_to_dest_selected: {md_pas_to_dest_selected}")
+
             print(f"value_taxi_to_dest_selected: {value_taxi_to_dest_selected}")
-            print(f"taxi.cash: {taxi.cash}")
+            print(f"cash_value: {cash_value}")
+            print(f"fuel_value: {fuel_value}")
             print(f"total_value: {total_value}")
 
             print("--------------------------------------------------")
 
-        return value_taxi_to_dest_selected +
+        return total_value
 
 
 class AgentMinimax(Agent):

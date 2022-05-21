1caca0601b00946d5e0d8232738003ca37d0df4c
Yair Nahum <yair.y.nahum@intel.com>
2022-05-22 02:23:10 +0300

    hw2 1.1 :
    1. increase the value of immeidate cash by 2* cash value
    2. gas margin to 3
    3. if no selection between passengesr, select 1

--- hw2/code/submission.py
+++ hw2/code/submission.py
@@ -18,8 +18,8 @@ class AgentGreedyImproved(AgentGreedy):
         other_taxi_id = (taxi_id + 1) % 2
         other_taxi = env.get_taxi(other_taxi_id)
 
-        cash_value = taxi.cash - other_taxi.cash
-        fuel_value = taxi.fuel - other_taxi.fuel
+        cash_value = 2*(taxi.cash - other_taxi.cash)
+        fuel_value = 2*(taxi.fuel - other_taxi.fuel)
         total_value = cash_value + fuel_value
 
         survival_value = 0
@@ -29,7 +29,7 @@ class AgentGreedyImproved(AgentGreedy):
 
         cost_taxi_to_gas0 = manhattan_distance(taxi.position, gas_0.position)
         cost_taxi_to_gas1 = manhattan_distance(taxi.position, gas_1.position)
-        if (cost_taxi_to_gas0 > (taxi.fuel-2)) and (cost_taxi_to_gas1 > (taxi.fuel-2)):
+        if (cost_taxi_to_gas0 > (taxi.fuel-3)) and (cost_taxi_to_gas1 > (taxi.fuel-3)):
             survival_value -= 10
             if debug_prints:
                 print(f"cost_taxi_to_gas0: {cost_taxi_to_gas0}")
@@ -107,8 +107,8 @@ class AgentGreedyImproved(AgentGreedy):
                 elif opt_p2_closer_to_taxi_than_other_taxi:
                     select_dest = 2
                 else:
-                    select_dest = 0
-                    print("*** no selection ** value is 0")
+                    select_dest = 1
+                    #print("*** no selection ** value is 0")
 
                 if select_dest == 1:
                     taxi_pas_selected = opt_pas_1
@@ -125,7 +125,7 @@ class AgentGreedyImproved(AgentGreedy):
                     remaining_cost = md_pas_to_dest_selected + md_taxi_to_pass_selected
                     value_taxi_to_dest_selected = cash_at_drop - remaining_cost
                 else:
-                    value_taxi_to_dest_selected = 0
+                    value_taxi_to_dest_selected = random.uniform(-0.5, 0.5)
 
         total_value += value_taxi_to_dest_selected
 

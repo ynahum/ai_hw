e4ab819464cd1c15f0ce6bd3fd866875ab762776
Yair Nahum <yair.y.nahum@intel.com>
2022-06-28 17:03:41 +0300

    hw3 dry knn vs id3

---
+++ hw3/AI_HW3_CODE/ID3/vornoy.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors
+
+n_neighbors = 1
+
+
+
+h = .02  # step size in the mesh
+
+# set point and labels
+X = np.array([[2, 2], [1, 1]])
+y = np.array([0, 1])
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.vlines(1.5,0,3, colors='g')
+    plt.scatter([1.25], [2], cmap=cmap_bold, marker='x')
+    plt.title("True f = ID3 != KNN")
+    plt.grid()
+
+plt.show()
